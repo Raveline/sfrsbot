f19c7bfc5232a121bf0235c850ab7ec27af66c31
Raveline <eraveline@gmail.com>
2015-07-14 14:27:01 +0200

    Missing import

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import random
+import dateutil.parser
 
 from twitter import Twitter, OAuth
 from model import Quote, Base
